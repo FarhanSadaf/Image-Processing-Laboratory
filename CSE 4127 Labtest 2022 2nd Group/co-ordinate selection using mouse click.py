ab11aee4fb1ddd85c3f9fe7d3365db8c7a08449d
Farhan Sadaf <52249052+FarhanSadaf@users.noreply.github.com>
2022-06-29 14:57:48 +0600

    Labtest

---
+++ CSE 4127 Labtest 2022 2nd Group/co-ordinate selection using mouse click.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 18:49:01 2022
+
+@author: Sunanda Das
+"""
+
+import cv2
+  
+
+
+def click_event(event, x, y, flags, params):
+ 
+    # checking for left mouse clicks
+    if event == cv2.EVENT_LBUTTONDOWN:
+ 
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+ 
+        # displaying the coordinates
+        # on the image window
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, str(x) + ',' +
+                    str(y), (x,y), font,
+                    1, (255, 0, 0), 2)
+        cv2.imshow('image', img)
+ 
+    # checking for right mouse clicks    
+    if event==cv2.EVENT_RBUTTONDOWN:
+ 
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+ 
+        # displaying the coordinates
+        # on the image window
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        b = img[y, x, 0]
+        g = img[y, x, 1]
+        r = img[y, x, 2]
+        cv2.putText(img, str(b) + ',' +
+                    str(g) + ',' + str(r),
+                    (x,y), font, 1,
+                    (255, 255, 0), 2)
+        cv2.imshow('image', img)
+ 
+
+   
+img = cv2.imread('input2.png', 0)
+cv2.imshow('image', img)   
+cv2.setMouseCallback('image', click_event)  
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
